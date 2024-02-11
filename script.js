3e0587a9e548b9302b5cb0732a4378cb84049ea5
Samuel <samuferfort@gmail.com>
2024-02-11 16:15:48 +0100

    Cleaned calculateResult function and added styles

--- script.js
+++ script.js
@@ -4,16 +4,16 @@
 const input = document.querySelector(".input");
 const addButton = document.querySelector(".adding");
 const buttons = document.querySelectorAll("button");
-const result = document.querySelectorAll(".equal");
+const result = document.querySelectorAll(".op");
 const clearInput = document.querySelector(".clear");
 const operators = ['+', '-', '÷', 'x'];
 // Change the input depending on button clicked
 buttons.forEach((element) => {
     element.addEventListener("click", () => {
-        input.innerHTML += element.innerHTML;
-        
+        input.innerHTML = input.innerHTML == '0' ? element.innerHTML 
+        : input.innerHTML + element.innerHTML; 
     });      
-     })
+})
 
 // Operations
 const add = (a, b) => parseFloat(a) + parseFloat(b);
@@ -36,7 +36,7 @@ function findOperator(inputString) {
 }
 
 
-function calculateTest () {
+function calculateTest (e) {
     const expression = input.innerHTML;
     const { operator, count } = findOperator(expression);
     
@@ -46,31 +46,41 @@ function calculateTest () {
     console.log(firstOperand)
     let secondOperand = output[1];
     console.log(secondOperand)
+    let operatorTwo = e.target.innerHTML;
     if (count > 1) {     
         
         secondOperand = output[1].split(operators)
-        
-    }else {
-        secondOperand = output[1]
     }
     console.log(secondOperand)
     if (!secondOperand) {
         return;
     }
-    if (!operator) {
-        input.innerHTML = "no operator!";
-    }
+
     let calculation;
     switch (operator) {
         case '+':
-            calculation = add(firstOperand, secondOperand);       
+            calculation = add(firstOperand, secondOperand);
+            break;
+        case 'x':
+            calculation = multiply(firstOperand, secondOperand);
+            break;
+        case '÷':
+            if (secondOperand === 0){
+                alert("Can't divide by 0");
+            }
+            calculation = divide(firstOperand, secondOperand);
+            break;
+        case '-':
+            calculation = subtract(firstOperand, secondOperand);
+            break;              
     }
+    console.log(operatorTwo)
     if (count > 1){
-    input.innerHTML = calculation;
+    input.innerHTML = calculation + operatorTwo
     } else {
         input.innerHTML = calculation;
     }    
 }
 // Calculate result and display output
-result.forEach((element) => element.addEventListener("click", calculateTest));        
-clearInput.addEventListener("click", () => input.innerHTML = "");
+result.forEach((element) => element.addEventListener("click", (e) => calculateTest(e)))        
+clearInput.addEventListener("click", () => input.innerHTML = "0");
